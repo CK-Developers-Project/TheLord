e89196f9135e926ee0c5700602f4a8ebc4de129c
dek0058 <47653276+dek0058@users.noreply.github.com>
2020-10-06 18:50:00 +0900

    [6주차] WorkTimer(증축시간) 추가

---
+++ Assets/Scripts/UI/MainLobby/WorkTimer.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+using System;
+
+public class WorkTimer : MonoBehaviour
+{
+    [SerializeField] TextMeshPro WorkTimeText;
+
+    [HideInInspector] public bool IsComplete { get; set; }
+
+    DateTime targetTime;
+
+    IEnumerator Runnable()
+    {
+        while (!IsComplete)
+        {
+            TimeSpan current = targetTime - DateTime.Now;
+            WorkTimeText.SetText(string.Format("{0:D2}:{1:D2}", current.Minutes, current.Seconds));
+            yield return null;
+        }
+    }
+
+
+    private void Start()
+    {
+        targetTime = DateTime.Now + new TimeSpan(0, 5, 10);
+        StartCoroutine(Runnable());
+    }
+
+}
