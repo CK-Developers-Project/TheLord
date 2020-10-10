d3b05666efb2ee56631e669f4bc706e85ca5283f
dek0058 <editionunit@gmail.com>
2020-10-10 17:56:58 +0900

    [6주차] 스파인 에셋 추가

--- Assets/Scripts/GameMode/IntroGameMode.cs
+++ Assets/Scripts/GameMode/IntroGameMode.cs
@@ -1,17 +1,8 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using UnityEngine.InputSystem;
 
-[ExecuteInEditMode]
 public class IntroGameMode : BaseGameMode
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        Keyboard a = Keyboard.current;
-        a.SetIMEEnabled ( true );
-        
-        //IMECompositionMode.Auto;
-    }
+    
 }
