6e4b4ebafb244f8515fcde4176536514ce15c675
dek0058 <47653276+dek0058@users.noreply.github.com>
2020-11-18 17:34:33 +0900

    [11주차] 네트워크 로그인 작업

--- Assets/Scripts/Net/Handler/LoginHandler.cs
+++ Assets/Scripts/Net/Handler/LoginHandler.cs
@@ -30,16 +30,16 @@ namespace Developers.Net.Handler
             switch((NextAction)response.ReturnCode)
             {
                 case NextAction.LoginSuccess:
-
+                    // TODO : 내 메인로비로 이동
                     break;
                 case NextAction.LoginFailed:
-
+                    BasePage.OnMessageBox("로그인에 실패하셨습니다.", true, null, "확인");
                     break;
                 case NextAction.UserCreateFail:
-
+                    BasePage.OnMessageBox("ID길이가 잘못 되었습니다.\n(2글자 ~ 10글자)", true, null, "확인");
                     break;
                 case NextAction.UserInfoCreate:
-
+                    // TODO : 종족선택창으로 이동
                     break;
             }
         }
