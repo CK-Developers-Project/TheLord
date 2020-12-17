2086b74b55484ac4614b32db300d1a0bacf7205c
dek0058 <47653276+dek0058@users.noreply.github.com>
2020-12-17 19:19:21 +0900

    [15주차] 건물정보 UI 작업
    * 테스트 유닛 프리펩 추가

---
+++ Assets/Scripts/UI/MainLobby/BuildingInfoPopup.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using TMPro;
+
+[ExecuteInEditMode]
+public class BuildingInfoPopup : MonoBehaviour
+{
+    public Sprite[] sprites;
+    int index = 0;
+
+    [SerializeField] AspectRatioFitter aspectRatioFitter;
+    [SerializeField] Image image;
+
+
+    public bool isSwitch = false;
+
+
+    private void Update()
+    {
+        if(isSwitch)
+        {
+            aspectRatioFitter.enabled = false;
+            image.sprite = sprites[index++];
+            image.SetNativeSize();
+            
+
+            isSwitch = false;
+        }
+    }
+
+
+}
