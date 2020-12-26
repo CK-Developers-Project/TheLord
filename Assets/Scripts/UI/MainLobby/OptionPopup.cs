1bffd2b3e04430a7b9325587e716447df054989c
cmd3689 <smartsilver123@gmail.com>
2020-12-27 04:44:00 +0900

    [16주차] 옵션 추가

---
+++ Assets/Scripts/UI/MainLobby/OptionPopup.cs
@@ -0,0 +1,20 @@
+﻿using UnityEngine;
+using TMPro;
+
+public class OptionPopup : MonoBehaviour
+{
+    public void Active()
+    {
+        gameObject.SetActive(!gameObject.activeSelf);
+    }
+
+    public void Logout()
+    {
+
+    }
+
+    public void EndGame()
+    {
+        Application.Quit();
+    }
+}
